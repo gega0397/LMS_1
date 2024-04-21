dc06b4c52bbf51417e89c4940c5aa67597712344
gega0397 <g_kandelaki@cu.edu.ge>
2024-04-21 23:50:44 +0400

    updated models

--- faculty/models.py
+++ faculty/models.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from .choices import USER_TYPE_CHOICES
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, blank=True, null=True)
-    is_authorized = models.BooleanField(default=False)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, blank=True, null=True,verbose_name=_("User Type"))
+    is_authorized = models.BooleanField(default=False, verbose_name=_("Is Authorized"))
 
     def __str__(self):
         return self.username
@@ -18,38 +19,62 @@ class CustomUser(AbstractUser):
     def is_lecturer(self):
         return self.user_type == 'student'
 
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+
 
 class Subject(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    syllabus = models.FileField(upload_to='syllabus/')
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+    description = models.TextField(verbose_name=_("Description"))
+    syllabus = models.FileField(upload_to='syllabus/', verbose_name=_("Syllabus"))
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = _('Subject')
+        verbose_name_plural = _('Subjects')
 
 class Faculty(models.Model):
-    name = models.CharField(max_length=100)
-    subjects = models.ManyToManyField(Subject)
-    lectures = models.ManyToManyField(CustomUser)
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+    subjects = models.ManyToManyField(Subject, related_name="faculty", verbose_name=_("Subjects"))
+    lectures = models.ManyToManyField(CustomUser, related_name="faculty", verbose_name=_("Lecturers"))
 
+    class Meta:
+        verbose_name = _('Faculty')
+        verbose_name_plural = _('Faculties')
 
 class Classroom(models.Model):
-    subjects = models.ManyToManyField(Subject)
-    lecturer = models.ManyToManyField(CustomUser)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name=_("Subjects"))
+    lecturer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name=_("Lecturers"))
 
+    class Meta:
+        verbose_name = _('Classroom')
+        verbose_name_plural = _('Classrooms')
 
 class SudentFaculty(models.Model):
-    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name=_("Student"), null=True, blank=True)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, verbose_name=_("Faculty"))
 
+    class Meta:
+        verbose_name = _('Student Faculty')
+        verbose_name_plural = _('Student Faculties')
 
-class LectureFaculty(models.Model):
-    lecture = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    subjects = models.ManyToManyField(Subject)
-    has_permission = models.BooleanField(default=False)
+class LectureSubject(models.Model):
+    lecturer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name=_("Lecturer"))
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name=_("Subjects"))
+    has_permission = models.BooleanField(default=False, verbose_name=_("Has Permission"))
+
+    class Meta:
+        verbose_name = _('Lecture Faculty')
+        verbose_name_plural = _('Lecture Faculties')
 
 
 class StudentSubject(models.Model):
-    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name=_("Student"))
+    subject = models.ForeignKey(Classroom, on_delete=models.CASCADE, verbose_name=_("Subject"))
+
+    class Meta:
+        verbose_name = _('Student Subject')
+        verbose_name_plural = _('Student Subjects')
