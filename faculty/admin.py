15d689a59495056a29f2767fa985bc4716b4d6c9
gega0397 <g_kandelaki@cu.edu.ge>
2024-04-23 12:27:53 +0400

    added basic admin forms and updated models

--- faculty/admin.py
+++ faculty/admin.py
@@ -9,9 +9,25 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ["email", "username", 'user_type', 'first_name', 'last_name', 'is_authorized']
+    list_display = ["email", 'user_type', 'first_name', 'last_name', 'is_authorized']
     ordering = ['date_joined']
 
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_authorized', 'user_type',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'first_name',
+                'last_name', 'password1', 'password2',
+                'is_authorized', 'user_type', ),
+        }),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
--- faculty/models.py
+++ faculty/models.py
@@ -21,7 +21,7 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
 
     def is_student(self):
         return self.user_type == 'student'
@@ -37,7 +37,7 @@ class CustomUser(AbstractUser):
 class Subject(models.Model):
     name = models.CharField(max_length=100, verbose_name=_("Name"))
     description = models.TextField(verbose_name=_("Description"))
-    syllabus = models.FileField(upload_to='syllabus/', verbose_name=_("Syllabus"))
+    syllabus = models.FileField(upload_to='syllabus/', verbose_name=_("Syllabus"), blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -50,27 +50,38 @@ class Subject(models.Model):
 class Faculty(models.Model):
     name = models.CharField(max_length=100, verbose_name=_("Name"))
     subjects = models.ManyToManyField(Subject, related_name="faculty", verbose_name=_("Subjects"))
-    lectures = models.ManyToManyField(CustomUser, related_name="faculty", verbose_name=_("Lecturers"))
+    lectures = models.ManyToManyField(CustomUser, related_name="faculty",
+                                      limit_choices_to={'user_type': 'lecturer'},
+                                      verbose_name=_("Lecturers"))
 
     class Meta:
         verbose_name = _('Faculty')
         verbose_name_plural = _('Faculties')
 
+    def __str__(self):
+        return self.name
+
+
 
 class Classroom(models.Model):
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name=_("Subjects"))
-    lecturer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name=_("Lecturers"))
+    lecturer = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
+                                 limit_choices_to={'user_type': 'lecturer'}, verbose_name=_("Lecturers"))
     is_full = models.BooleanField(default=False, verbose_name=_("Is Full"))
-    is_active = models.BooleanField(default=False, verbose_name=_("Is Active"))
+    is_active = models.BooleanField(default=True, verbose_name=_("Is Active"))
     max_students = models.IntegerField(verbose_name=_("Max Students"), default=MAX_CLASSROOM_SIZE)
 
     class Meta:
         verbose_name = _('Classroom')
         verbose_name_plural = _('Classrooms')
 
+    def __str__(self):
+        return f'{self.subject} {self.lecturer}'
+
 
 class SudentFaculty(models.Model):
-    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name=_("Student"))
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
+                                limit_choices_to={'user_type': 'student'}, verbose_name=_("Student"))
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE, verbose_name=_("Faculty"))
     status = models.CharField(max_length=20, choices=USER_STATUS_CHOICES, default='inactive',
                               verbose_name=_("User Type"))
@@ -78,6 +89,9 @@ class SudentFaculty(models.Model):
     class Meta:
         verbose_name = _('Student Faculty')
         verbose_name_plural = _('Student Faculties')
+
+    def __str__(self):
+        return f"{self.student}: {self.faculty}"
 
 
 # class LectureSubject(models.Model):
@@ -91,9 +105,13 @@ class SudentFaculty(models.Model):
 
 
 class StudentSubject(models.Model):
-    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name=_("Student"))
+    student = models.ForeignKey(CustomUser, on_delete=models.CASCADE,
+                                limit_choices_to={'user_type': 'student'}, verbose_name=_("Student"))
     classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, verbose_name=_("Subject"))
 
     class Meta:
         verbose_name = _('Student Subject')
         verbose_name_plural = _('Student Subjects')
+
+    def __str__(self):
+        return f"{self.student}: {self.classroom}"
