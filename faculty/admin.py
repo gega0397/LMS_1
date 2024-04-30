fb096ae8545b5f8940dd80afe329680a621035be
gega0397 <g_kandelaki@cu.edu.ge>
2024-04-30 16:09:54 +0400

    updated admin files for USERS app

--- faculty/admin.py
+++ faculty/admin.py
@@ -1,38 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
-from faculty.forms import CustomUserCreationForm, CustomUserChangeForm
 from faculty.models import CustomUser, Subject, Faculty, Classroom, StudentSubject, StudentFaculty, Homework
-
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ["email", 'user_type', 'first_name', 'last_name', 'is_authorized']
-    ordering = ['date_joined']
-    list_filter = ['is_authorized', 'user_type']
-
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name',)}),
-        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_authorized', 'user_type',)}),
-        ('Important dates', {'fields': ('last_login', 'date_joined',)}),
-    )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': (
-                'email', 'first_name',
-                'last_name', 'password1', 'password2',
-                'is_authorized', 'user_type',),
-        }),
-    )
-
-
-# admin.site.register(CustomUser, CustomUserAdmin)
 
 
 @admin.register(Subject)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.forms import CustomUserCreationForm, CustomUserChangeForm
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ["email", 'user_type', 'first_name', 'last_name', 'is_authorized']
+    ordering = ['date_joined']
+    list_filter = ['is_authorized', 'user_type']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_authorized', 'user_type',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'first_name',
+                'last_name', 'password1', 'password2',
+                'is_authorized', 'user_type',),
+        }),
+    )
